c7e5551d9b3c0e1db9db23d3f66b737d81b73edb
Jed Fox <git@jedfox.com>
2022-12-22 12:38:24 -0500

    Initial Commit

---
+++ Screens/ScreensApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreensApp.swift
+//  Screens
+//
+//  Created by Jed Fox on 2022-12-22.
+//
+
+import SwiftUI
+
+@main
+struct ScreensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
